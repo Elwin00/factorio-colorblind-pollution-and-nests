0fe86660d4c0db563af0b9162db611a4608f4794
Elwin <elwin@elwinarium.net>
2026-06-29 09:30:43 +0200

    formatting

--- constants.lua
+++ constants.lua
@@ -7,5 +7,5 @@ function C.dbg(msg)
   if C.DEBUG then log("[colorblind] " .. msg) end
 end
 
-C.BULK_INSERTER_TINT    = {r = 0.4, g = 0.8, b = 0.45, a = 1}
+C.BULK_INSERTER_TINT = {r = 0.4, g = 0.8, b = 0.45, a = 1}
 return C
